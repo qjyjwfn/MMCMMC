d1ac2dd974e6bf6790982d12f311034e498aa9ea
qjyjwfn <y6xhtxpvfp@privaterelay.appleid.com>
2026-06-13 20:44:00 +0800

    Delete Spotify directory

--- Spotify/Spotify-json.js
+++
@@ -1,9 +0,0 @@
-console.log(`spotify-json-2025.06.20`);
-let url = $request.url;
-if (url.includes('com:443')) {
-    url = url.replace(/com:443/, 'com');
-}
-if (url.includes('platform=iphone')) {
-    url = url.replace(/platform=iphone/, 'platform=ipad');
-}
-$done({ url });
